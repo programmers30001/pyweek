5df4673bdd633d7b2646cc9ec6bd43a5875d1668
JOKERadaw <valerioman05@gmail.com>
2023-03-27 21:30:50 +0200

    made it 10000x faster

--- game/screens/game_screen.py
+++ game/screens/game_screen.py
@@ -1,6 +1,8 @@
 import pygame
 from pygame import gfxdraw
 import numpy as np
+from PIL import Image
+import visionlight
 class GameScreen:
     def __init__(self, width, height):
         self.width = width
@@ -12,19 +14,14 @@ class GameScreen:
 
         self.play_text = self.font.render("having fun?", True, (255, 255, 255))
         self.radius=40
+        self.img = Image.open("spacestation.jpg")
+        self.arr = np.array(self.img)
     def display(self):
-        img = pygame.image.load("spacestation.jpg").convert()
 
+        self.screen.fill([0,0,0])
         a, b = pygame.mouse.get_pos()
-        self.screen.blit(img,(0,0))
-        for i in range(self.width):
-            for j in range(self.height):
-                if ((i-a)**2+(j-b)**2)**0.5<self.radius:
-                    self.drawpixel(pygame.Surface.get_at(self.screen, (i, j))[:3],(i,j))
-
-                else:
-                    self.drawpixel([0,0,0],(i,j))
-
+        if a<self.arr.shape[0]-self.radius and b<self.arr.shape[1]+self.radius:
+            visionlight.see(self.width,self.height,self.screen,a,b,self.radius,self.arr)
 
     def drawpixel(self,color, pos):
         pygame.draw.line(self.screen, color, pos, pos)
