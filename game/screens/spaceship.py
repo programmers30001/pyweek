97113fd9889a118cbf2ad29904a882e2a0b686c5
JOKERadaw <valerioman05@gmail.com>
2023-03-29 16:28:37 +0200

    f

--- game/screens/spaceship.py
+++ game/screens/spaceship.py
@@ -10,22 +10,28 @@ class GameScreen:
         self.font = pygame.font.Font(None, 50)
 
         self.arr = []
-
-        background = pygame.image.load("images/spacestation.png")
+        self.backgrounds=[]
+        background = pygame.image.load("images/first_room.png")
+        self.backgrounds.append(background)
         self.background = pygame.transform.scale(background, (width, height))
 
         key = pygame.image.load("images/key.png")
         self.key = pygame.transform.scale(key, (25, 30))
         self.battery=10000000
         self.used=0
+        self.xkey=random.randint(0,self.width-30)
+        self.ykey=0
+        self.gotkey=0
     def display(self,lighton,radius):
         self.screen.fill([0, 0, 0])
 
         # put objects onto the background before calling array3d()
-        self.background.blit(self.key, (0, 0))
+        if not self.gotkey:
+            self.background.blit(self.key, (self.xkey, self.ykey))
         color = (255*self.used/self.battery, 255*(1-self.used/self.battery), 0)
 
         # Drawing Rectangle
+        pygame.draw.rect(self.screen, color, pygame.Rect(self.width*1/3, 0, self.width/3*(1-self.used/self.battery), 60))
         pygame.draw.rect(self.screen, color, pygame.Rect(self.width*1/3, 0, self.width/3*(1-self.used/self.battery), 60))
 
         # use native pygame functions to convert to a pixel array
@@ -44,3 +50,5 @@ class GameScreen:
                 if ((i - a) ** 2 + (j - b) ** 2) ** 0.5 < radius:
                     pygame.draw.line(screen, pixels[i][j], (i, j), (i, j))
 
+    def clickedkey(self,a,b):
+        pass
